e7a3f763c92e0e743f0077ca90aca47c04203ed0
Brad Hambrice <hambrice@uab.edu>
2018-01-26 07:37:58 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,10 +1,10 @@
 class Ship
     attr_accessor :name, :type, :booty
     @@all = []
-    def initialize(name, type, booty)
-      @name = name
-      @type = type
-      @booty = booty
+    def initialize(hash)
+      @name = hash[:name]
+      @type = hash[:type]
+      @booty = hash[:booty]
       @@all << self
     end
     def self.all
