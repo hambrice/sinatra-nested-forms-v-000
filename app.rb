fb4d6b3331ef6b587b121ab0b963a340d31f32b5
Brad Hambrice <hambrice@uab.edu>
2018-01-26 08:09:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ module FormsLab
     post '/pirates' do
       #binding.pry
       @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
-      @ship_1 = Ship.new(params["ship_name_1"],params["pirate"]["ships"][0]["type"], params["pirate"]["ships"][0]["booty"])
+      @ship_1 = Ship.new(params["pirate"]["ships"][0]["name"],params["pirate"]["ships"][0]["type"], params["pirate"]["ships"][0]["booty"])
       @ship_2 = Ship.new(params["pirate"]["ships"][1]["name"], params["pirate"]["ships"][1]["type"], params["pirate"]["ships"][1]["booty"])
       erb :show
     end
